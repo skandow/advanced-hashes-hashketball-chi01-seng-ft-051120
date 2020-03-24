4cad89491fe8f2c7a8757ac041b7e170fd10c77a
Steven Kandow <kando1sr@gmail.com>
2020-03-24 19:18:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,12 @@
 def game_hash
   game_results = {
     :home => {
-      :team_name => "Brooklyn Nets"
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"]
+      :players => [
+        {
+          :player_name => 
+        }]
     }
 
 
