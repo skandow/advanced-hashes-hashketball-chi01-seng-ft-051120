c2abc018beb640a95d8ce763824ebada2e58aa48
Steven Kandow <kando1sr@gmail.com>
2020-03-24 19:21:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,8 +5,7 @@ def game_hash
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
       :players => [
-        {
-          :player_name => "Alan Anderson",
+        {:player_name => "Alan Anderson",
           :number => 0, 
           :shoe => 16,
           :points => 22, 
@@ -14,8 +13,17 @@ def game_hash
           :assists => 12,
           :steals => 3,
           :blocks => 1,
-          :slam_dunks => 
-        }]
+          :slam_dunks => 1},
+        {:player_name => "Alan Anderson",
+          :number => 0, 
+          :shoe => 16,
+          :points => 22, 
+          :rebounds => 12,
+          :assists => 12,
+          :steals => 3,
+          :blocks => 1,
+          :slam_dunks => 1}
+        ]
     }
 
 
