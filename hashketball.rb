a6ff8cbde53f68b1eee2a7bcc8f5f25935d48306
Steven Kandow <kando1sr@gmail.com>
2020-03-24 19:21:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,7 +15,7 @@ def game_hash
           :blocks => 1,
           :slam_dunks => 1},
         {:player_name => "Reggie Evans",
-          :number => 0, 
+          :number => 30, 
           :shoe => 16,
           :points => 22, 
           :rebounds => 12,
