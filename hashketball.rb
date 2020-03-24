b37a0ea4710f51e6346c23524808bfd4dfadde96
Steven Kandow <kando1sr@gmail.com>
2020-03-24 19:19:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -11,7 +11,8 @@ def game_hash
           :shoe => 16
           :points => 22 
           :rebounds => 12
-          :assists => 
+          :assists => 12
+          :steals => 
         }]
     }
 
