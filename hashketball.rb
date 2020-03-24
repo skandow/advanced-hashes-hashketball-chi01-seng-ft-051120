774b20a45d35de43c627749789e058ed19bc2322
Steven Kandow <kando1sr@gmail.com>
2020-03-24 19:21:27 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -14,7 +14,7 @@ def game_hash
           :steals => 3,
           :blocks => 1,
           :slam_dunks => 1},
-        {:player_name => "Alan Anderson",
+        {:player_name => "Reggie Evans",
           :number => 0, 
           :shoe => 16,
           :points => 22, 
